d7c1840cb793398bf5fca7322d33cef2572a5df8
luishmr0 <luchiitoz@gmail.com>
2017-10-01 00:31:41 -0500

    home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,23 @@
 <template>
     <section class="hero is-black is-fullheight">
-  <div class="hero-body  ">
+  <div class="hero-body">
     <div class="container">
-      <h1 class="title">
-        Hola!
+       <img src="https://lhmr0.000webhostapp.com/images/darth.png" alt="hmr0" height="155" width="153">
+      <h1 class="title page-title">
+        hmr0
       </h1>
       <h2 class="subtitle">
-        soy Luis, software engineering, football, bookworm, gamer, movies
+      software engineering, football, games, movies
       </h2>
       <h2 class="subtitle">
-        encuéntrame en
+        encuéntrame @
       </h2>
       <a href="http://twitter.com/luis_hmr0" target="_blank">                    
              <b-icon
                 pack="fa"
                 icon="twitter"
                 size="is-large"
+                class="fa"
                 type="is-light">
             </b-icon></a>
             <a href="http://github.com/luishmr0" target="_blank">                    
@@ -23,6 +25,7 @@
                 pack="fa"
                 icon="github"
                 size="is-large"
+                class="fa"
                 type="is-light">
             </b-icon></a>
             <a href="https://www.facebook.com/luis.hmro" target="_blank">                    
@@ -30,6 +33,7 @@
                 pack="fa"
                 icon="facebook"
                 size="is-large"
+                class="fa"
                 type="is-light">
             </b-icon></a>
             <a href="http://steamcommunity.com/id/luishmr0/" target="_blank">                    
@@ -37,6 +41,7 @@
                 pack="fa"
                 icon="steam"
                 size="is-large"
+                class="fa"
                 type="is-light">
             </b-icon></a>
     </div>
@@ -61,6 +66,22 @@ export default {
     -webkit-background-size:cover;
     -moz-background-size:cover;
     -o-background-size:cover;
-    background-size:cover;    
+    background-size:cover;  
   }
+  .page-title{
+    font-family: "Open Sans",sans-serif;
+    margin: 10px 0;
+    font-size: 4rem;
+    letter-spacing: -2px;
+    font-weight: 700;    
+}
+  .page-title:hover, .subtitle:hover{
+    transition: 1.2s;
+    color: #c62525;
+  }
+  .fa:hover {
+    transition: 0.7s;
+    transform: scale(1.4);
+    color: #c62525;
+}
 </style>
